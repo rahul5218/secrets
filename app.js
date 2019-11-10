2e1a4f36307fda1158088afdd40e2366e57c313e
Rahul Shanker <rahulshanker52@gmail.com>
2019-11-10 17:01:44 +0530

    Add procfile change Callback URL and port

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://ancient-river-59894.herokuapp.com/auth/google/secrets",
     userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
@@ -78,7 +78,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/secrets",
+    callbackURL: "https://ancient-river-59894.herokuapp.com/auth/facebook/secrets",
     profileFields : ['id', 'photos', 'name', 'displayName', 'gender', 'profileUrl', 'email']
   },
   function(accessToken, refreshToken, profile, done) {
@@ -278,6 +278,12 @@ app.get("/contact",function(req,res){
   res.render("contact");
 });
 
-app.listen(3000,function(){
-  console.log("sever started on port 3000");
+app.get("/privacy",function(req,res){
+  res.render("privacy");
 });
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
